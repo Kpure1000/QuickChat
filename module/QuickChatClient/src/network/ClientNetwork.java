088402749314559f2802994f4b4cbf3125e250b4
Kpurek <ktsq2000@126.com>
2020-07-11 20:07:08 +0800

    commit from Desk

--- module/QuickChatClient/src/network/ClientNetwork.java
+++ module/QuickChatClient/src/network/ClientNetwork.java
@@ -8,42 +8,23 @@ import java.net.Socket;
 /**
  * clientNetwork
  * <p>
-<<<<<<< HEAD
- *      网络层，负责与服务器通讯，确保连接，处理连接异常
-=======
- * 网络层，
- * 负责与服务器通讯，
- * 确保连接，
- * 处理连接异常，
- * 不含任何业务逻辑
->>>>>>> ee3b490580dc08ad9d1ad3d47fb83376249a7c18
+ * 网络层，负责与服务器通讯，确保连接，处理连接异常
  * </p>
  *
  * @author Kpurek
  * @version 1.0
  */
-public class ClienNetWork {
+public class ClientNetwork {
 
-<<<<<<< HEAD
-
-    public ClienNetWork(){
-        isConnected=false;
-    }
-
-
-    /**
-     * 传入回调实例
-     * @param netCallBack 回调接口
-=======
-    public ClienNetWork() {
+    public ClientNetwork() {
         isConnected = false;
     }
+
 
     /**
      * 传入回调实例
      *
      * @param netCallBack
->>>>>>> ee3b490580dc08ad9d1ad3d47fb83376249a7c18
      */
     public void setNetCallBack(NetCallBack netCallBack) {
         this.netCallBack = netCallBack;
