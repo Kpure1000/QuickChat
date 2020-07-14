581c6681d7f50cd58e08aa6160b15a37ce9408a5
Kpurek <ktsq2000@126.com>
2020-07-14 23:44:35 +0800

    change listener

--- module/QuickChatClient/src/network/ListenManager.java
+++ module/QuickChatClient/src/network/ListenManager.java
@@ -7,7 +7,13 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 监听线程管理
+ * <p>
+ *     废弃这个功能，
+ *     为了确保线程安全，减少工作量，
+ *     Listener只允许存在一个
+ * </p>
  */
+@Deprecated
 public class ListenManager {
     private ThreadPoolExecutor listenPool = new ThreadPoolExecutor(1, 3,
             300, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(32), new ThreadPoolExecutor.DiscardPolicy());
